9ed8ea90401753c2358b710364202fbad44ef802
“MilosMachara“ <milosmachara@gmail.com>
2025-10-24 19:47:23 +0200

    uprava textu o mne a pridanie citatu

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ $(function () {
     window.location.href = "index.html"; // Presmerovanie na domovskú stránku
   });
 });
-
+window.addEventListener("scroll", () => {
+  const mainImg = document.querySelector(".main-img img");
+  let scrollY = window.scrollY;
+  
+  // rýchlosť efektu – môžeš doladiť (0.3 = jemný pohyb)
+  mainImg.style.transform = `translateY(${scrollY * 0.3}px)`;
+});
