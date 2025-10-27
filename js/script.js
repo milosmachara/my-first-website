861c91702e8704a32f6e601bf569fc23febe3f90
“MilosMachara“ <milosmachara@gmail.com>
2025-10-27 16:30:17 +0100

    opticka uprava kodu

--- js/script.js
+++ js/script.js
@@ -4,26 +4,22 @@ $(function () {
   });
 });
 
-
-const faders = document.querySelectorAll('.fade-in'); // vyber všetky elementy s triedou fade-in
+const faders = document.querySelectorAll(".fade-in"); // vyber všetky elementy s triedou fade-in
 
 const appearOptions = { threshold: 0.1 }; // element sa považuje za "viditeľný", keď je 10% v okne prehliadača
 
 const appearOnScroll = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('visible');
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
       observer.unobserve(entry.target); // už nepotrebujeme sledovať tento element, prestane sledovať, aby sa animácia nespúšťala opakovane.
     }
   });
 }, appearOptions);
 
-faders.forEach(fader => appearOnScroll.observe(fader));
+faders.forEach((fader) => appearOnScroll.observe(fader));
 
- 
-  // Po načítaní stránky vždy scroll na vrch
-window.onload = function() {
-    window.scrollTo(0, 0);
+// Po načítaní stránky vždy scroll na vrch
+window.onload = function () {
+  window.scrollTo(0, 0);
 };
-
-
