de6c8a2981510f34408453a7b23daef58068d920
“MilosMachara“ <milosmachara@gmail.com>
2025-11-10 21:43:57 +0100

    oprava responsivity

--- js/script.js
+++ js/script.js
@@ -23,3 +23,22 @@ faders.forEach((fader) => appearOnScroll.observe(fader));
 window.onload = function () {
   window.scrollTo(0, 0);
 };
+
+// Nájdeme element s triedou "iam"
+const quote = document.querySelector(".iam");
+
+// Uložíme si texty
+const slovakText =
+  '„Nikdy nie je neskoro byť tým, kým si mal byť.“<span class="eliot"><br>-George Eliot</span>';
+const englishText =
+  '“It is never too late to be who you might have been.”<span class="eliot"><br>-George Eliot</span>';
+
+// Keď prejdem myšou – zmeň text
+quote.addEventListener("mouseenter", () => {
+  quote.innerHTML = englishText;
+});
+
+// Keď kurzor odíde – vráť späť
+quote.addEventListener("mouseleave", () => {
+  quote.innerHTML = slovakText;
+});
